5e315a22d92a04686a0b8d8bc65cecf88574bab2
Lexi <mlexifarris@gmail.com>
2023-10-24 18:54:25 -0500

    added route, seed and model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Produt.create(name: "dahlia" price: 49 image_url:"https://www.edenbrothers.com/cdn/shop/products/dahlia-lindas-baby-1.jpg" description:"A beautifu flower to decorate your life. Great for fall")
+
+Product.create(name: "orchid" price: 70 image_url: "https://i.etsystatic.com/6077424/r/il/2fc1d3/2144099262/il_794xN.2144099262_gdcp.jpg" description: "Perfect flower to decorate with in the summer. Low maintaince but doesn't like to flower if you aren't meticilous with its care ")
