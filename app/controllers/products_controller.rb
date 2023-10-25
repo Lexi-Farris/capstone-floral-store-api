599991b85f530d70d8e0d8bd8a6d70800e38666e
Lexi <mlexifarris@gmail.com>
2023-10-24 21:37:21 -0500

    specified to show certain information in json page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,14 +9,21 @@ class ProductsController < ApplicationController
   #   render json: flower2.as_json
   # end
 
-  def orchid_lovers         #returns single product to page
-    flower = Product.find_by(id: params["id"])
-    render json: flower.as_json
+  # def orchid_lovers         #returns single product to page
+  #   flower = Product.find_by(id: params["id"])
+  #   render json: flower.as_json
+  # end
+
+  #remember to add .json at the end of the url for this to work: http://localhost:3000//liliopsidas/1.json
+  
+  def orchid_lovers
+    @product = Product.find_by(id: params["id"])
+    render template: "products/show"
   end
 
   def dahlia_lovers
-    flower2 = Product.find_by(name: params["name"])
-    render json: flower2.as_json
+    @product = Product.find_by(name: params["name"])
+    render template: "products/show"
   end
   
 end
